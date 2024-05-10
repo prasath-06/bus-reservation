239f6b4775afdaf220b6fab68d9230acb9e060e7
prasath-06 <169019617+prasath-06@users.noreply.github.com>
2024-05-10 10:22:14 +0530

    Update Booking.java

---
+++ Booking.java
@@ -0,0 +1,41 @@
+package bookings;
+import java.util.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+public class Booking {
+	String passengerName;
+	int busNo;
+	Date date;
+	Booking(){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter name of passenger:");
+		passengerName=sc.next();
+		System.out.println("enter bus no:");
+		busNo=sc.nextInt();
+		System.out.println("enter date dd-mm-yyyy");
+		String dateInput=sc.next();
+		SimpleDateFormat dateFormat=new SimpleDateFormat("dd-MM-yyyy");
+		
+		try {
+			date=dateFormat.parse(dateInput);
+		}catch(ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	public boolean isAvailable(ArrayList<Booking> bookings, ArrayList<Bus> buses) {
+		int capacity=0;
+		for(Bus bus:buses) {
+			if(bus.getBusNo()==busNo){
+				capacity=bus.getCapacity();
+			}
+		}
+		int booked=0;
+		for(Booking b:bookings) {
+			if(b.busNo==busNo && b.date.equals(date)) {
+				booked++;
+			}
+		}
+		return booked<capacity?true:false;
+	}
+	
+}
